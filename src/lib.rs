5cb2f13204673381e9e28ec8ec52a0436eab2a01
chesedo <pieter@chesedo.me>
2024-06-18 16:15:00 +0100

    Parse a simple request input

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,27 @@
 use syn::parse::{Parse, ParseStream, Result};
 
 #[derive(Debug, PartialEq, Eq)]
-struct RequestBuilder;
+struct RequestBuilder {
+    method: String,
+    uri: String,
+}
 
-impl Parse for RequestBuilder {
-    fn parse(input: ParseStream) -> Result<Self> {
-        Ok(RequestBuilder)
+impl RequestBuilder {
+    fn new(input: &str) -> Self {
+        let mut headers = [httparse::EMPTY_HEADER; 16];
+        let mut req = httparse::Request::new(&mut headers);
+        let res = req.parse(input.as_bytes()).unwrap();
+
+        let method = req.method.unwrap();
+        let uri = req.path.unwrap();
+
+        RequestBuilder {
+            method: method.to_string(),
+            uri: uri.to_string(),
+        }
     }
 }
+
 #[cfg(test)]
 mod tests {
     use syn::parse_quote;
@@ -15,9 +29,25 @@ mod tests {
     use super::*;
 
     #[test]
+    #[ignore = "httparse always requires the version. Thus, will have to make own parser first"]
     fn basic() {
-        let actual: RequestBuilder = parse_quote! {};
-        let expected = RequestBuilder;
+        let actual: RequestBuilder = RequestBuilder::new(r#"GET /health"#);
+        let expected = RequestBuilder {
+            method: "GET".to_string(),
+            uri: "/health".to_string(),
+        };
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn basic_with_version() {
+        let actual: RequestBuilder = RequestBuilder::new(r#"GET /health HTTP/1.1"#);
+        let expected = RequestBuilder {
+            method: "GET".to_string(),
+            uri: "/health".to_string(),
+        };
+
         assert_eq!(actual, expected);
     }
 }
