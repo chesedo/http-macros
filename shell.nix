7a97500dc9b29c8e81c2bb414675fa1e8824b8fb
chesedo <pieter@chesedo.me>
2024-06-19 09:37:16 +0100

    Nix dev shell setup

---
+++ shell.nix
@@ -0,0 +1,19 @@
+let
+  moz_overlay = import (builtins.fetchTarball
+    "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
+  # Pin to stable from https://status.nixos.org/
+  nixpkgs = import (fetchTarball
+    "https://github.com/NixOS/nixpkgs/archive/752c634c09ceb50c45e751f8791cb45cb3d46c9e.tar.gz") {
+      overlays = [ moz_overlay ];
+    };
+in with nixpkgs;
+stdenv.mkDerivation {
+  name = "moz_overlay_shell";
+  nativeBuildInputs = with nixpkgs; [ pkg-config openssl ];
+  buildInputs = with nixpkgs; [
+    ((rustChannelOf { channel = "1.79.0"; }).rust.override {
+      extensions = [ "rust-src" ];
+    })
+    cargo-watch
+  ];
+}
