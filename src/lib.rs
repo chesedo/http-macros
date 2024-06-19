28495e9e7416446963df7862bf70bb9fc08ca1c5
chesedo <pieter@chesedo.me>
2024-06-19 06:49:46 +0100

    Parse request headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-use syn::parse::{Parse, ParseStream, Result};
+use std::collections::HashMap;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Default)]
 struct RequestBuilder {
     method: String,
     uri: String,
+    headers: HashMap<String, String>,
 }
 
 impl RequestBuilder {
@@ -14,27 +15,37 @@ impl RequestBuilder {
 
         let method = req.method.unwrap();
         let uri = req.path.unwrap();
+        let mut headers = HashMap::new();
+
+        for header in req.headers {
+            if !header.name.is_empty() {
+                headers.insert(
+                    header.name.to_string(),
+                    std::str::from_utf8(&header.value).unwrap().to_string(),
+                );
+            }
+        }
 
         RequestBuilder {
             method: method.to_string(),
             uri: uri.to_string(),
+            headers,
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use syn::parse_quote;
-
     use super::*;
 
     #[test]
     #[ignore = "httparse always requires the version. Thus, will have to make own parser first"]
     fn basic() {
-        let actual: RequestBuilder = RequestBuilder::new(r#"GET /health"#);
+        let actual: RequestBuilder = RequestBuilder::new("GET /health");
         let expected = RequestBuilder {
             method: "GET".to_string(),
             uri: "/health".to_string(),
+            ..Default::default()
         };
 
         assert_eq!(actual, expected);
@@ -42,12 +53,59 @@ mod tests {
 
     #[test]
     fn basic_with_version() {
-        let actual: RequestBuilder = RequestBuilder::new(r#"GET /health HTTP/1.1"#);
+        let actual: RequestBuilder = RequestBuilder::new("GET /health HTTP/1.1");
         let expected = RequestBuilder {
             method: "GET".to_string(),
             uri: "/health".to_string(),
+            ..Default::default()
         };
 
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn with_headers() {
+        let actual: RequestBuilder = RequestBuilder::new(
+            r#"GET /health HTTP/1.1
+Host: localhost:8000
+Accept: application/json
+"#, // TODO: remove the new line
+        );
+        let expected = RequestBuilder {
+            method: "GET".to_string(),
+            uri: "/health".to_string(),
+            headers: {
+                let mut headers = HashMap::new();
+                headers.insert("Host".to_string(), "localhost:8000".to_string());
+                headers.insert("Accept".to_string(), "application/json".to_string());
+                headers
+            },
+        };
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    #[ignore = "httparse always requires the version. Thus, will have to make own parser first"]
+    fn with_malformed_headers() {
+        let actual: RequestBuilder = RequestBuilder::new(
+            r#"GET /health
+Host: localhost:8000
+Accept
+"#, // TODO: remove the new line
+        );
+        let expected = RequestBuilder {
+            method: "GET".to_string(),
+            uri: "/health".to_string(),
+            headers: {
+                let mut headers = HashMap::new();
+                headers.insert("Host".to_string(), "localhost:8000".to_string());
+                headers.insert("Accept".to_string(), "application/json".to_string());
+                headers
+            },
+        };
+
+        // TODO: Test for error somehow
         assert_eq!(actual, expected);
     }
 }
